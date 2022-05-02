7ff44f23952313fec60029e298cd5fadef7fa099
Phatdev <tuphat3456tt@gmail.com>
2022-05-02 13:41:50 +0700

    #001 update

--- test/main.js
+++ test/main.js
@@ -1,4 +1,4 @@
-const { FileSystem } = require('../lib/index.bundle');
+const { FileSystem } = require('../lib/index.folody');
 const { Client, Intents } = require('discord.js');
 
 const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES]});
@@ -8,5 +8,5 @@ FileSystem.setup('commands', client, '!', function() {
     client.on('ready', () => {
         console.log('Ready!');
     })
-    client.login("TOKEN");
+    client.login("OTU0NTg4NTQ0NzEyNzA4MTQ2.YjVT3A.UXcXydizKUJOWhRVL0USy009bJo");
 });
